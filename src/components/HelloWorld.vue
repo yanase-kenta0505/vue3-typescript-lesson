02dd6f71636742c09f8f0758b56d990536f6af5f
yanase-kenta0505 <ynsknt0505@gmail.com>
2022-01-22 22:15:51 +0900

    interface

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <!-- <h1>{{ msg }}</h1> -->
+    <h1 >{{ book.title }}/{{ book.author }}/{{ book.year }}</h1>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+interface Book{
+  title: string;
+  author: string;
+  year: number;
+}
 export default defineComponent({
   name: "HelloWorld",
   data() {
     return {
-      msg: "Hello TypeScript" as string | number
+      // msg: "Hello TypeScript" as string | number
+      book: {
+        title: 'Vue 3 Guide',
+        author: 'Vue Team',
+        year: 2020
+      } as Book
     };
   },
   // computed: {
@@ -20,9 +30,10 @@ export default defineComponent({
   //   },
   // },
   methods:{
-    changeMsg(msg:string | number){
-      return this.msg = msg
-    }
+    // changeMsg(msg:string | number){
+    //   return this.msg = msg
+    // }
+ 
   }
 });
 </script>
